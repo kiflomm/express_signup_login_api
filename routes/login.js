1d5a4f8e260a9d36997985cb64d6d7d59c192272
Kiflom Berihu Abay <kiflomberihu2024@gmail.com>
2024-06-16 17:52:42 +0000

    Added login route

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const bcryptjs = require('bcryptjs')
+const User = require('../models/user')
 const router = express.Router()
 
-router.post('/', (req,res)=>{
-    res.send("This is from the login route")
+router.post('/', async (req,res)=>{
+    const {email,password} = req.body
+    const user = await User.findOne({email})                    //  checking if the user is already registered 
+    if(user){
+        if(await bcryptjs.compare(password,user.password)){     //  verifying the password
+            return res.status(200).json({name:user.name})
+        }else{
+            return res.status(404).json({wrongPassword:true})
+        }
+    }else{
+        return res.status(404).json({userNotFound:true})
+    }
 })
 
 module.exports = router
